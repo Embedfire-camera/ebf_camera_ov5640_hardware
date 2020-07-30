c45e31dd6d5aca8d8ac244ba0c989ab1498db777
nebulamagic <892461474@qq.com>
2020-07-30 16:55:13 +0800

    目录整理

---
+++ OV5640封装库/原理图+PCB 封装库（AD+PADS版本）/PADS/OV5640_PCB.d
@@ -0,0 +1,22 @@
+*PADS-LIBRARY-PCB-DECALS-V9*
+
+SOT23-5          I 1000  1000  0 2 1 0 5 1 0
+TIMESTAMP 2012.07.19.08.06.30
+-60   209   0     0 100   10    1 32 35 "Regular <Romansim Stroke Font>"
+PART-TYPE
+-60   209   0     0 100   10    1 0 34 "Regular <Romansim Stroke Font>"
+REF-DES
+OPEN   2 10 0 -1
+-15   -60  
+15    -60  
+T37.5  50    37.5  50    1
+T0     50    0     50    2
+T-37.5 50    -37.5 50    3
+T-37.5 -50   -37.5 -50   4
+T37.5  -50   37.5  -50   5
+PAD 0 3 P 0    
+-2 20  RF  0   90   50 0  
+-1 0   R  
+0  0   R  
+
+*END*
